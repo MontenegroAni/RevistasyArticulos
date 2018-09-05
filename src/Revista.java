e2d0722aa864793dcf8af78a45fc3ca64ba55ea7
Ana Paula Montenegro <38986710+MontenegroAni@users.noreply.github.com>
2018-09-05 02:18:47 -0300

    Add files via upload
    Octavo modelo de parcial de 111mil. El error que lanzaba fue resuelto.

--- src/Revista.java
+++ src/Revista.java
@@ -7,31 +7,30 @@ public class Revista {
 	private int ejemplar;
 
 	public Revista(String titulo, int ejemplar){
-	this.titulo= titulo;
-	this.ejemplar= ejemplar;
-	this.elementos= new ArrayList<Articulo>();
+		this.titulo= titulo;
+		this.ejemplar= ejemplar;
+		this.elementos= new ArrayList<Articulo>();
 	}
 
 	public boolean addElemento(Articulo articulo){
-	return elementos.add(articulo);
+		return elementos.add(articulo);
 	}
 
 	public Articulo getArticuloEnPosicion(int posicion){
-	int posicion;
-	for(int i=0; i<elementos.size(); i++){ 
-	if(i==posicion-1)
-	return elementos.get(i);
-	}
-	return null;
+		for(int i=0; i<elementos.size(); i++){ 
+			if(i==posicion-1)
+				return elementos.get(i);
+		}
+		return null;
 	}
 
 	public int getCantidadArticulosDeTema(String tema){
-	int cantidad=0;
-	for(Articulo a: elementos){
-	if(a.contieneTema(tema)){
-	cantidad++;
-	}
-	}
+		int cantidad=0;
+		for(Articulo a: elementos){
+			if(a.contieneTema(tema)){
+				cantidad++;
+			}
+		}
 	return cantidad;
 	}
 
